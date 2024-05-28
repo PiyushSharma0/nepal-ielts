740a823b29a93c26f3dfb1aeca486ca936b8320b
PiyushSharma0 <piyush.sharma.code@gmail.com>
2024-05-28 09:23:15 +0530

    Minor

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -6,34 +6,29 @@ import "slick-carousel/slick/slick-theme.css";
 
 const testimonials = [
   {
-    text: "With the assistance of Online Class Nepal, I achieved a score of 7.5. Despite struggling with weak writing skills, Santosh sir provided continuous guidance throughout the sessions. Through group division and speaking practice sessions, I was able to improve significantly and attain a score of 7.5",
-    name: "Bibita Joshi",
-    position: "Founder of Varibo",
-    image: "/Bibita Joshi.png", // Replace with the path to the testimonial image
-  },
-  {
-    text: "With the assistance of Online Class Nepal, I achieved a score of 7.5. Despite struggling with weak writing skills, Santosh sir provided continuous guidance throughout the sessions. Through group division and speaking practice sessions, I was able to improve significantly and attain a score of 7.5",
-    name: "Arpan KC",
-    position: "Founder of Varibo",
-    image: "/Arpan KC.png", // Replace with the path to the testimonial image
-  },
-  {
-    text: "With the assistance of Online Class Nepal, I achieved a score of 7.5. Despite struggling with weak writing skills, Santosh sir provided continuous guidance throughout the sessions. Through group division and speaking practice sessions, I was able to improve significantly and attain a score of 7.5",
-    name: "Arnab Singh",
-    position: "Founder of Varibo",
-    image: "/Arnab Singh.png", // Replace with the path to the testimonial image
-  },
-  {
-    text: "With the assistance of Online Class Nepal, I achieved a score of 7.5. Despite struggling with weak writing skills, Santosh sir provided continuous guidance throughout the sessions. Through group division and speaking practice sessions, I was able to improve significantly and attain a score of 7.5",
-    name: "Alex Shreshta",
-    position: "Founder of Varibo",
-    image: "/Alex Shrestha.jpeg", // Replace with the path to the testimonial image
-  },
-  {
-    text: "With the assistance of Online Class Nepal, I achieved a score of 7.5. Despite struggling with weak writing skills, Santosh sir provided continuous guidance throughout the sessions. Through group division and speaking practice sessions, I was able to improve significantly and attain a score of 7.5",
+    text: "I never imagined I'd achieve a band score of 8, but thanks to Santosh sir's teaching style, it became possible. He focuses on all students, teaches in a friendly manner, and guides us through difficult problems, ensuring everyone excels in the IELTS exam. Plus, the unlimited mock tests prepared me thoroughly. I'm grateful for their support.",
     name: "Swagat Regmi",
-    position: "Founder of Varibo",
-    image: "/Swagat Regmi.jpeg", // Replace with the path to the testimonial image
+    image: "/Swagat Regmi.jpeg",
+  },
+  {
+    text: "Thanks to Online Class Nepal, I achieved an incredible band score of 8 in the IELTS exam! Honestly, I never thought I could reach this level, but their amazing online study materials and personalized guidance from the instructor made it possible. I'm really grateful to them for helping me succeed.",
+    name: "Arpan KC",
+    image: "/Arpan KC.png",
+  },
+  {
+    text: "Thanks to Online Class Nepal, I achieved a Band 7.5 in my IELTS exam! Their unlimited mock tests and recorded videos were a huge help, especially when I couldn't attend live sessions. The teacher was brilliant and guided me through the writing section as I found writing tasks a bit difficult. I'm grateful for their support in my success.Definitely Online Class Nepal.",
+    name: "Alex Shreshta",
+    image: "/Alex Shrestha.jpeg"
+  },
+  {
+    text: "Thanks to Online Class Nepal, achieving a score of 7.5 in my IELTS exam became a reality! Their exam booking service, coupled with the convenience of the exam book dating, made the entire process hassle-free. Additionally, the personalized guidance provided by my instructor played a crucial role in my success. I am grateful to Online Class Nepal for their exceptional services and dedicated assistance throughout my preparation journey!",
+    name: "Arnab Singh",
+    image: "/Arnab Singh.png",
+  },
+  {
+    text: "With the assistance of Online Class Nepal, I achieved a score of 7.5.  Despite struggling with weak writing skills, Santosh sir provided continuous guidance throughout the sessions. Through group division and speaking practice sessions, I was able to improve significantly and attain a score of 7.5.",
+    name: "Pranil Maharjan",
+    image: "/Pranil Maharjan.jpeg",
   },
 ];
 
@@ -47,7 +42,7 @@ const Testimonials = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 5000,
     arrows: false,
     beforeChange: (oldIndex, newIndex) => setCurrentIndex(newIndex),
   };
@@ -65,15 +60,15 @@ const Testimonials = () => {
   };
 
   return (
-    <section className="bg-gray-100 py-12">
+    <section className="bg-gray-100 py-12 h-[560px]">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-semibold text-center mb-8" style={{color: '#525252'}}>
+        <h2 className="text-3xl font-semibold text-center mb-8 text-[#525252] ">
           Testimonials
         </h2>
         <Slider {...settings}>
           {testimonials.map((testimonial, index) => (
             <div key={index} className="text-center px-24">
-              <div className="relative mb-4 text-xl md:text-2xl" style={{color: '#101827'}}>
+              <div className="relative mb-4 text-xl md:text-2xl">
                 <span className="absolute left-0 -top-2 text-blue-500 text-8xl">
                   ❝
                 </span>
@@ -85,7 +80,7 @@ const Testimonials = () => {
             </div>
           ))}
         </Slider>
-        <div className="flex justify-center mt-8 space-x-4">
+        <div className="flex justify-center items-center gap-3 mt-4 space-x-2">
           <div className="relative w-16 h-16">
             <Image
               src={getTestimonialImage(-2)}
@@ -110,19 +105,18 @@ const Testimonials = () => {
               alt={getTestimonialName(0)}
               width={128}
               height={128}
-              className="rounded-full"
-              style={{ padding: "8px" }} // Add padding
+              className="rounded-full p-2"
             />
             <div
-              className="absolute inset-0 rounded-full border-4 border-blue-500"
+              className="absolute inset-0 rounded-full border-4 border-[#1273EB]"
               style={{
                 clipPath:
-                  "polygon(50% 100%, 0% 100%, 0% 0%, 100% 0%, 100% 75%, 50% 75%)",
-                padding: "8px", // Add padding
+                  "polygon(50% 100%, 0% 100%, 0% 0%, 100% 0%, 100% 60%, 50% 50%)",
+                padding: "8px",
               }}
             />
             <div className="relative inset-x-0 bottom-0 text-center my-4">
-              <p className="font-semibold text-lg" style={{color: '#1E1F4B'}}>{getTestimonialName(0)}</p>
+              <p className="font-semibold text-lg text-[#1E1F4B]">{getTestimonialName(0)}</p>
             </div>
           </div>
           <div className="relative w-24 h-24">
