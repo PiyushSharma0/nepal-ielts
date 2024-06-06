e812b457fa5200bd2c878928bcdb851aef9ec499
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2024-06-07 01:31:50 +0530

    Revert "refactor: Update image file extensions for consistency in Footer and Header components"
    This reverts commit f660f3a6057b33ee09bebbbba270d6bdcdb8a529.

--- components/TawkToChat.jsx
+++ components/TawkToChat.jsx
@@ -13,7 +13,7 @@ const TawkToChat = () => (
           (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src=process.env.REACT_APP_TAWK_TO_SRC;
+            s1.src='https://embed.tawk.to/66604f5f981b6c564778c6f9/1hvk3c3p3';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
